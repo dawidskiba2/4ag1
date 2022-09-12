09d4c38ec0fb9a0a5c15e4db56557198f6c0de06
dawidskiba2 <d.skiba420@wp.pl>
2022-09-12 12:55:50 +0200

    main

---
+++ ppai/app1/menu.js
@@ -0,0 +1,164 @@
+let menuArr = [
+ {
+  "id": 1,
+  "Nazwa": "Schabowy z kością, młodymi ziemniakami w koprze i kapustą zasmażaną",
+  "Skladniki": "Schabowy  *  młode ziemniaki  *  kapusta zasmażana",
+  "Cena": 56,
+  "Zdjecie": "schabowy.jpg",
+  "Rodzaj": "Danie główne",
+  "Typ": "N"
+ },
+ {
+  "id": 2,
+  "Nazwa": "Żeberka wieprzowe marynowane w sosie barbecue",
+  "Skladniki": "Żeberka wieprzowe  *  sos barbecue  *  pieczone ziemniaki  *  czerwoną kapusta",
+  "Cena": 52,
+  "Zdjecie": "zeberka.jpg",
+  "Rodzaj": "Danie główne",
+  "Typ": "N"
+ },
+ {
+  "id": 3,
+  "Nazwa": "Tagliatelle z grillowaną kiełbasą chorizo",
+  "Skladniki": "Tagliatelle  *  grillowana kiełbasą chorizo  *  krewetkami  *  groszek cukrowy ",
+  "Cena": 49,
+  "Zdjecie": "tagiatele.jpg",
+  "Rodzaj": "Danie główne",
+  "Typ": "N"
+ },
+ {
+  "id": 4,
+  "Nazwa": "Risotto z polędwiczką wieprzową",
+  "Skladniki": "Risotto  *  polędwiczka wieprzowa  *  suszone pomidory  *  cukinia  *  białe wino",
+  "Cena": 52,
+  "Zdjecie": "risotto.jpg",
+  "Rodzaj": "Danie główne",
+  "Typ": "N"
+ },
+ {
+  "id": 5,
+  "Nazwa": "Filet z kurczaka z frytkami",
+  "Skladniki": "Filet z kurczaka  *  frytki  * czarna soczewica  *  suszone pomidory  *  szparagi  *  brokuł  *  sos limonkowo śmietanowy",
+  "Cena": 53,
+  "Zdjecie": "filet.jpg",
+  "Rodzaj": "Danie główne",
+  "Typ": "N"
+ },
+ {
+  "id": 6,
+  "Nazwa": "Paella z owocami morza",
+  "Skladniki": "Paela  *  krewetki  *  małże  *  homar",
+  "Cena": 66,
+  "Zdjecie": "paella.jpg",
+  "Rodzaj": "Danie główne",
+  "Typ": "N"
+ },
+ {
+  "id": 7,
+  "Nazwa": "Zupa pomidorowa z pestkami z dyni",
+  "Skladniki": "Pomidory  *  makaron  *  śmietana  *  pestki z dyni",
+  "Cena": 21,
+  "Zdjecie": "tomatoesup.jpg",
+  "Rodzaj": "Zupy",
+  "Typ": "N"
+ },
+ {
+  "id": 8,
+  "Nazwa": "Rosół z kury",
+  "Skladniki": "Wywar z mięsa drobiowego  *  mięso drobiowe  *  marchewka",
+  "Cena": 23,
+  "Zdjecie": "bulion.jpg",
+  "Rodzaj": "Zupy",
+  "Typ": "N"
+ },
+ {
+  "id": 9,
+  "Nazwa": "Zupa z soczewicy z papryką",
+  "Skladniki": "Soczewica  *  papryka  *  ziemniaki  *  tłuszcz roślinny",
+  "Cena": 20,
+  "Zdjecie": "soczewica.jpg",
+  "Rodzaj": "Zupy",
+  "Typ": "W"
+ },
+ {
+  "id": 10,
+  "Nazwa": "Żurek z jajkiem i kiełbasą",
+  "Skladniki": "Zakwas chlebowy  *  kiełbasa  *  cebula  *  kiełbasa wiejska",
+  "Cena": 26,
+  "Zdjecie": "zurek.jpg",
+  "Rodzaj": "Zupy",
+  "Typ": "N"
+ },
+ {
+  "id": 11,
+  "Nazwa": "Zupa z dyni",
+  "Skladniki": "Dynia  *  kluseczki wegetariańskie",
+  "Cena": 22,
+  "Zdjecie": "dynia.jpg",
+  "Rodzaj": "Zupy",
+  "Typ": "W"
+ },
+ {
+  "id": 12,
+  "Nazwa": "Zupa grochowa",
+  "Skladniki": "Groch  *  kiełbasa  *  ziemniaki  *  marchewka",
+  "Cena": 19,
+  "Zdjecie": "grochowa.jpg",
+  "Rodzaj": "Zupy",
+  "Typ": "N"
+ },
+ {
+  "id": 13,
+  "Nazwa": "Tatar wołowy",
+  "Skladniki": "Mięso wołowe  *  jajko",
+  "Cena": 16,
+  "Zdjecie": "tatar.jpg",
+  "Rodzaj": "Przystawki",
+  "Typ": "N"
+ },
+ {
+  "id": 14,
+  "Nazwa": "Sałatka z burakiem czerwonym",
+  "Skladniki": "Sałata  *  burak czerwony  *  ser  *  szpinak",
+  "Cena": 21,
+  "Zdjecie": "salatka1.jpg",
+  "Rodzaj": "Przystawki",
+  "Typ": "N"
+ },
+ {
+  "id": 15,
+  "Nazwa": "Taca serów",
+  "Skladniki": "Sery różnych gatunków",
+  "Cena": 69,
+  "Zdjecie": "sery.jpg",
+  "Rodzaj": "Przystawki",
+  "Typ": "N"
+ },
+ {
+  "id": 16,
+  "Nazwa": "Dzwonek z tuńczyka",
+  "Skladniki": "Tuńczyk  *  oliwa",
+  "Cena": 32,
+  "Zdjecie": "tunczyk.jpg",
+  "Rodzaj": "Przystawki",
+  "Typ": "N"
+ },
+ {
+  "id": 17,
+  "Nazwa": "Piadina",
+  "Skladniki": "Piadina  *  pomidor  *  rukola",
+  "Cena": 27,
+  "Zdjecie": "piadina.jpg",
+  "Rodzaj": "Przystawki",
+  "Typ": "N"
+ },
+ {
+  "id": 18,
+  "Nazwa": "Borówki",
+  "Skladniki": "Borówki amerykańskie",
+  "Cena": 11,
+  "Zdjecie": "borowki.jpg",
+  "Rodzaj": "Przystawki",
+  "Typ": "W"
+ }
+]

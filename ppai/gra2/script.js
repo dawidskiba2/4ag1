2c243200d7d7fecfd8eefe0b6b659dd25c2586de
dawidskiba2 <d.skiba420@wp.pl>
2022-10-26 17:08:19 +0200

    26.10

--- ppai/gra2/script.js
+++ ppai/gra2/script.js
@@ -1,4 +1,6 @@
 var character = document.getElementById('character');
+var block = document.getElementById('block');
+var counter = 0;
 
 function jump(){
     if(character.classList == 'animate') {return}
@@ -7,3 +9,21 @@ function jump(){
         character.classList.remove('animate');
     }, 300);
 }
+
+var checkDead = setInterval(function(){
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+
+    if(blockLeft<20 && blockLeft>-20 && characterTop >= 130){
+        block.style.animation = "none";
+        alert("game over. score: "+Math.floor(counter/100));
+        counter = 0;
+        block.style.animation = "block 1s infinite linear";
+    }
+    else{
+        counter++;
+        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+    }
+    
+}, 10)
+
